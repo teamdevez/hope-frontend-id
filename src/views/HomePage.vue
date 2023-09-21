1bb92433875d09b535840b0d89c46e1eecd8a0bd
onlynuttakorn <onlynuttakorn@gmail.com>
2023-09-21 18:38:42 +0700

    first commit

---
+++ src/views/HomePage.vue
@@ -0,0 +1,181 @@
+<template>
+  <b-container fluid>
+    <b-row class="justify-content-center m-4">
+      <b-img
+        fluid
+        src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Service_mark.svg"
+        :width="200"
+      >
+      </b-img>
+    </b-row>
+    <b-row class="justify-content-center m-4">
+      <b-col cols="3" align-self="center">
+        <b-card class="text-center">
+          <b-card-body class="p-0">
+            <h1>{{ this.thaiTime }}</h1>
+            <b-card-text>{{ this.thaiDate }}</b-card-text>
+          </b-card-body>
+        </b-card>
+      </b-col>
+    </b-row>
+    <b-row class="justify-content-center m-4">
+      <!-- <b-col cols="12"> -->
+      <h2>ข้อมูลนักศึกษาปัจจุบัน</h2>
+      <!-- </b-col> -->
+    </b-row>
+    <b-row class="justify-content-center m-4">
+      <div class="mx-4">
+        <b-img
+          thumbnail
+          fluid
+          :src="
+            student && student.length > 7
+              ? `http://192.168.1.46:8085${student[8]}`
+              : 'https://picsum.photos/250/250/?image=54'
+          "
+          alt="Image 1"
+          :width="200"
+          class="h-100"
+        >
+        </b-img>
+      </div>
+      <div class="student-details">
+        <b-card>
+          <b-card-body>
+            <b-card-title
+              >สถานะ:
+              {{
+                this.student && this.student.length > 3 ? this.student[4] : ""
+              }}</b-card-title
+            >
+            <b-card-text
+              >วันเวลา:
+              {{
+                this.student && this.student.length > 2 ? this.student[3] : ""
+              }}</b-card-text
+            >
+            <hr />
+            <b-card-text
+              >ชื่อ:
+              {{
+                this.student && this.student.length > 1 ? this.student[2] : ""
+              }}</b-card-text
+            >
+            <b-card-text
+              >รหัสนักศึกษา:
+              {{
+                this.student && this.student.length > 0 ? this.student[1] : ""
+              }}</b-card-text
+            >
+          </b-card-body>
+        </b-card>
+      </div>
+    </b-row>
+    <hr />
+    <b-row class="justify-content-center m-4">
+      <h2>ข้อมูลนักศึกษาก่อนหน้า</h2>
+    </b-row>
+    <b-row class="justify-content-center m-4">
+      <b-card-group deck>
+        <b-card
+          v-for="(stu, i) in studentList"
+          :key="i"
+          :img-src="
+            stu[8]
+              ? `http://192.168.1.46:8085${student[8]}`
+              : 'https://picsum.photos/250/250/?image=54'
+          "
+          img-top
+          class="custom-image"
+        >
+          <b-card-text>รหัสนศ: {{ stu[1] }}</b-card-text>
+          <b-card-text>ชื่อ นามสกุล: {{ stu[2] }}</b-card-text>
+          <template #footer>
+            <small class="text-muted">{{ stu[3] }}</small>
+          </template>
+        </b-card>
+      </b-card-group>
+    </b-row>
+  </b-container>
+</template>
+<script>
+import moment from "moment-timezone";
+export default {
+  name: "HomePage",
+  data() {
+    return {
+      connection: null,
+      datetimeNow: null,
+      timezone: "Asia/Bangkok",
+      thaiTime: "",
+      thaiDate: "",
+      student: null,
+      studentList: []
+    };
+  },
+  created() {
+    this.connectToWebSocket();
+  },
+  mounted() {
+    this.datetimeNow = moment().tz(this.timezone);
+    // สร้าง interval สำหรับการอัปเดตเวลาทุก 1 วินาที
+    setInterval(this.updateThaiTime, 1000);
+    this.thaiDate = this.datetimeNow.format("DD/MM/YYYY");
+  },
+  methods: {
+    updateThaiTime() {
+      const now = moment().tz(this.timezone);
+      this.thaiTime = now.format("HH:mm:ss");
+    },
+    connectToWebSocket() {
+      console.log("Starting connection to WebSocket Server");
+      const _SELF = this;
+      const wsUrl = "ws://localhost:3001";
+      this.connection = new WebSocket(wsUrl);
+
+      this.connection.onmessage = function(event) {
+        let data = JSON.parse(event.data);
+        console.log("event", data);
+        if (data.length > 0) {
+          _SELF.student = data[0];
+          data.shift();
+          console.log("student", _SELF.student);
+        }
+        _SELF.studentList = data;
+      };
+      setInterval(() => {
+        if (this.connection.readyState !== WebSocket.OPEN) {
+          console.log("WebSocket connection is not open. Reconnecting...");
+          this.connectToWebSocket();
+        } else {
+          this.connection.send("ping");
+        }
+      }, 10000);
+      this.connection.onopen = function(event) {
+        console.log("Successfully connected to the echo websocket server...");
+      };
+      this.connection.onerror = function(event) {
+        console.error("WebSocket error:", event);
+        setTimeout(() => {
+          _SELF.connectToWebSocket();
+        }, 10000);
+      };
+    }
+  }
+};
+</script>
+<style>
+.box-time {
+  width: 30%;
+}
+.card.custom-image img {
+  width: 100%;
+  height: 100px;
+}
+.student-details {
+  width: 500px;
+}
+hr {
+  border: 1px solid #000000;
+}
+</style>
