e8e59bb1c23188cb8eb9905db3a1dbede6e628be
teamdevez <110016902+teamdevez@users.noreply.github.com>
2023-09-29 15:23:05 +0700

    แก้ไข Design

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,102 +1,166 @@
 <template>
-  <b-container fluid>
-    <b-row class="justify-content-center m-4">
-      <b-img
-        fluid
-        src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Service_mark.svg"
-        :width="200"
-      >
-      </b-img>
-    </b-row>
-    <b-row class="justify-content-center m-4">
-      <b-col cols="3" align-self="center">
-        <b-card class="text-center">
-          <b-card-body class="p-0">
-            <h1>{{ this.thaiTime }}</h1>
-            <b-card-text>{{ this.thaiDate }}</b-card-text>
-          </b-card-body>
-        </b-card>
-      </b-col>
-    </b-row>
-    <b-row class="justify-content-center m-4">
-      <!-- <b-col cols="12"> -->
-      <h2>ข้อมูลนักศึกษาปัจจุบัน</h2>
-      <!-- </b-col> -->
-    </b-row>
-    <b-row class="justify-content-center m-4">
-      <div class="mx-4">
-        <b-img
-          thumbnail
-          fluid
-          :src="
-            student && student.length > 7
-              ? `http://192.168.1.46:8085${student[8]}`
-              : 'https://picsum.photos/250/250/?image=54'
-          "
-          alt="Image 1"
-          :width="200"
-          class="h-100"
-        >
+  <div>
+    <b-container fluid>
+      <b-row class="justify-content-center m-4">
+        <b-img fluid :src="require('../../static/logo.png')" :width="200">
         </b-img>
+      </b-row>
+      <b-row class="justify-content-center">
+        <b-col cols="4">
+          <b-row class="justify-content-center m-4">
+            <b-col cols="12" align-self="center">
+              <b-card class="text-center">
+                <b-card-body class="p-0">
+                  <h1>{{ this.thaiTime }}</h1>
+                  <b-card-text>{{ this.thaiDate }}</b-card-text>
+                </b-card-body>
+              </b-card>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col cols="6">
+              <b-card bg-variant="success" text-variant="white">
+                <b-card-body class="p-0">
+                  <h6 class="text-center">ภายในวิทยาลัย (Check In)</h6>
+                  <h2 class="text-center mb-0">250</h2></b-card-body
+                >
+              </b-card>
+            </b-col>
+            <b-col cols="6" class="mb-3">
+              <b-card bg-variant="danger" text-variant="white">
+                <b-card-body class="p-0">
+                  <h6 class="text-center">นอกวิทยาลัย (Check Out)</h6>
+                  <h2 class="text-center mb-0">250</h2></b-card-body
+                >
+              </b-card>
+            </b-col>
+            <b-col cols="12">
+              <b-card bg-variant="info" text-variant="white">
+                <b-card-body class="p-0">
+                  <h6 class="text-center">นักศึกษาทั้งหมด</h6>
+                  <h2 class="text-center mb-0">250</h2></b-card-body
+                >
+              </b-card>
+            </b-col>
+          </b-row>
+        </b-col>
+        <b-col cols="6">
+          <b-row class="justify-content-center m-4">
+            <b-col cols="12">
+              <h2 class="text-center">
+                การบันทึกข้อมูลการเข้า-ออก ของนักศึกษาล่าสุด
+              </h2>
+            </b-col>
+          </b-row>
+          <b-row class="justify-content-center m-4">
+            <div class="mx-4">
+              <b-img
+                thumbnail
+                fluid
+                :src="
+                  student && student.length > 7
+                    ? `http://192.168.1.46:8085${student[8]}`
+                    : 'https://picsum.photos/250/250/?image=54'
+                "
+                alt="Image 1"
+                :width="200"
+                class="h-100"
+              >
+              </b-img>
+            </div>
+            <div class="student-details">
+              <b-card>
+                <b-card-body>
+                  <b-card-title
+                    >สถานะ:
+                    {{
+                      this.student && this.student.length > 3
+                        ? this.student[4]
+                        : ""
+                    }}</b-card-title
+                  >
+                  <b-card-text
+                    >วันเวลา:
+                    {{
+                      this.student && this.student.length > 2
+                        ? this.student[3]
+                        : ""
+                    }}</b-card-text
+                  >
+                  <hr />
+                  <b-card-text
+                    >ชื่อ:
+                    {{
+                      this.student && this.student.length > 1
+                        ? this.student[2]
+                        : ""
+                    }}</b-card-text
+                  >
+                  <b-card-text
+                    >รหัสนักศึกษา:
+                    {{
+                      this.student && this.student.length > 0
+                        ? this.student[1]
+                        : ""
+                    }}</b-card-text
+                  >
+                  <b-card-text
+                    >ชั้นปี:
+                    {{
+                      this.student && this.student.length > 0
+                        ? this.student[1]
+                        : ""
+                    }}</b-card-text
+                  >
+                  <b-card-text
+                    >รุ่นที่:
+                    {{
+                      this.student && this.student.length > 0
+                        ? this.student[1]
+                        : ""
+                    }}</b-card-text
+                  >
+                </b-card-body>
+              </b-card>
+            </div>
+          </b-row>
+        </b-col>
+      </b-row>
+
+      <hr />
+      <b-row class="justify-content-center m-4">
+        <h2>ข้อมูลนักศึกษาก่อนหน้า</h2>
+      </b-row>
+      <b-row class="justify-content-center m-4">
+        <b-card-group deck>
+          <b-card
+            v-for="(stu, i) in studentList"
+            :key="i"
+            :img-src="
+              stu[8]
+                ? `http://192.168.1.46:8085${student[8]}`
+                : 'https://picsum.photos/250/250/?image=54'
+            "
+            img-top
+            class="custom-image"
+          >
+            <b-card-text>รหัสนศ: {{ stu[1] }}</b-card-text>
+            <b-card-text>ชื่อ นามสกุล: {{ stu[2] }}</b-card-text>
+            <template #footer>
+              <small class="text-muted">{{ stu[3] }}</small>
+            </template>
+          </b-card>
+        </b-card-group>
+      </b-row>
+    </b-container>
+    <footer>
+      <div>
+        <h6 class="text-center">
+          Copyright © 2023 Cosma Solution CO. LTD
+        </h6>
       </div>
-      <div class="student-details">
-        <b-card>
-          <b-card-body>
-            <b-card-title
-              >สถานะ:
-              {{
-                this.student && this.student.length > 3 ? this.student[4] : ""
-              }}</b-card-title
-            >
-            <b-card-text
-              >วันเวลา:
-              {{
-                this.student && this.student.length > 2 ? this.student[3] : ""
-              }}</b-card-text
-            >
-            <hr />
-            <b-card-text
-              >ชื่อ:
-              {{
-                this.student && this.student.length > 1 ? this.student[2] : ""
-              }}</b-card-text
-            >
-            <b-card-text
-              >รหัสนักศึกษา:
-              {{
-                this.student && this.student.length > 0 ? this.student[1] : ""
-              }}</b-card-text
-            >
-          </b-card-body>
-        </b-card>
-      </div>
-    </b-row>
-    <hr />
-    <b-row class="justify-content-center m-4">
-      <h2>ข้อมูลนักศึกษาก่อนหน้า</h2>
-    </b-row>
-    <b-row class="justify-content-center m-4">
-      <b-card-group deck>
-        <b-card
-          v-for="(stu, i) in studentList"
-          :key="i"
-          :img-src="
-            stu[8]
-              ? `http://192.168.1.46:8085${student[8]}`
-              : 'https://picsum.photos/250/250/?image=54'
-          "
-          img-top
-          class="custom-image"
-        >
-          <b-card-text>รหัสนศ: {{ stu[1] }}</b-card-text>
-          <b-card-text>ชื่อ นามสกุล: {{ stu[2] }}</b-card-text>
-          <template #footer>
-            <small class="text-muted">{{ stu[3] }}</small>
-          </template>
-        </b-card>
-      </b-card-group>
-    </b-row>
-  </b-container>
+    </footer>
+  </div>
 </template>
 <script>
 import moment from "moment-timezone";
